17abb9246367bc1f8288f929deed60d5499af571
AdvTech <none>
2024-01-25 15:05:26 +0800

    Upload Code.

---
+++ siamban/models/neck/neck.py
@@ -0,0 +1,81 @@
+# Copyright (c) SenseTime. All Rights Reserved.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import torch
+import torch.nn as nn
+
+
+class AdjustLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(AdjustLayer, self).__init__()
+        self.downsample = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            )
+
+        # 修改部分,自注意力机制
+        self.sample = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels),
+            )
+        self.getQ = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels//8, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels//8),
+        )
+        self.getK = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels // 8, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels // 8),
+        )
+
+    def forward(self, x):
+        # 以下为修改部分
+        q = self.getQ(x)
+        k = self.getK(x)
+        B, C, h, w = q.shape
+
+        q = q.contiguous().view(B, C, h * w)
+        q = q.permute(0, 2, 1)
+        k = k.contiguous().view(B, C, h * w)
+        SAW = torch.softmax(torch.matmul(q, k), dim=1)
+
+        x0 = self.sample(x)
+        B, C, H, W = x0.shape
+        x0 = x0.contiguous().view(B, C, H * W)
+        x0 = torch.matmul(x0, SAW).view(B, C, H, W)
+
+        weightA = 0.5
+        XS =torch.add(weightA * x0, x)
+
+        x = self.downsample(XS)
+
+        if x.size(3) < 20:
+            l = 4
+            r = l + 7
+            x = x[:, :, l:r, l:r]
+        return x
+
+
+class AdjustAllLayer(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(AdjustAllLayer, self).__init__()
+        self.num = len(out_channels)
+        if self.num == 1:
+            self.downsample = AdjustLayer(in_channels[0], out_channels[0])
+        else:
+            for i in range(self.num):
+                self.add_module('downsample'+str(i+2),
+                                AdjustLayer(in_channels[i], out_channels[i]))
+
+    def forward(self, features):
+        if self.num == 1:
+            return self.downsample(features)
+        else:
+            out = []
+            for i in range(self.num):
+                adj_layer = getattr(self, 'downsample'+str(i+2))
+                out.append(adj_layer(features[i]))
+            return out
